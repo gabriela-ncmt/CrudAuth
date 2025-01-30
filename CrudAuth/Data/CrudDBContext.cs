34049182fae97d7f4ea97e2adf2127d1d21dd516
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2025-01-30 12:22:28 -0300

    feat: configuring database context to manage User and Product entities.

---
+++ CrudAuth/Data/CrudDBContext.cs
@@ -0,0 +1,20 @@
+﻿using CrudAuth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudAuth.Data
+{
+    /*
+     * CrudDBContext inherits from IdentityDbContext type of User
+     * which means that it already comes with Identity support
+     *  >>Authentication and User Management <<
+     *  AND we're including the DBSet of Product which will be used to do
+     *  CRUD operations in the Products
+    */
+    public class CrudDBContext : IdentityDbContext<User>
+    {
+        public CrudDBContext(DbContextOptions<CrudDBContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
