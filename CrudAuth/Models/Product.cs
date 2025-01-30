b62a0840f996d003e7e34eac5f4e916b0386aa80
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2025-01-30 12:16:01 -0300

    feat: creating entities: - Product - User

---
+++ CrudAuth/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace CrudAuth.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ CrudAuth/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CrudAuth.Models
+{
+    public class User : IdentityUser
+    {
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
