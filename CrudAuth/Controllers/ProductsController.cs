18cb7563256792ae67d0a98f0388fc4b5339853f
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2025-01-31 12:06:30 -0300

    feat: implementing CRUD Product on Controller

---
+++ CrudAuth/Controllers/ProductsController.cs
@@ -0,0 +1,89 @@
+﻿using CrudAuth.Data;
+using CrudAuth.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProductsController : ControllerBase
+    {
+        private readonly CrudDBContext _dbContext;
+        public ProductsController(CrudDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _dbContext.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var product = await _dbContext.Products.FindAsync(id);
+            if(product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<Product>> PostProduct(Product produt)
+        {
+            _dbContext.Products.Add(produt);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProductById), new {Id =  produt.Id}, produt);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+                return BadRequest();
+
+            _dbContext.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _dbContext.Products.FindAsync(id);
+            if(product == null)
+                return NotFound();
+
+            _dbContext.Products.Remove(product);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        public bool ProductExists(int id)
+        {
+            return _dbContext.Products.Any(p => p.Id == id);
+        }
+    }
+}
